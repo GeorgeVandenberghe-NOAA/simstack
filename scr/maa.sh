d94350725861749e87448f8143aa87dab51a70d4
george vandenberghe <george.vandenberghe@alogin02.acorn.wcoss2.ncep.noaa.gov>
2023-12-21 18:08:59 +0000

    removed wget of boost from maa.sh.   that violated the principle of separating gits and gets from the build and maa.sh does builds only

--- scr/maa.sh
+++ scr/maa.sh
@@ -198,7 +198,6 @@ sh -x ../cmake.sh
 cd $PW
 
 rm -rf  boost_1_68_0
-wget -nv https://boostorg.jfrog.io/artifactory/main/release/1.68.0/source/boost_1_68_0.tar.gz
 tar -xvf boost_1_68_0.tar.gz
 cd  boost_1_68_0
 pwd
