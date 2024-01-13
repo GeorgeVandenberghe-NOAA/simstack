0dfafecdddd340be701869db9f95c7ae850badac
George Vandenberghe <George.Vandenberghe@noaa.gov>
2024-01-13 12:56:57 +0000

    corrected bug in upp build

--- global.sorc/ufs_model.fd/FV3/upp/tests/cmake.raw.sh
+++ global.sorc/ufs_model.fd/FV3/upp/tests/cmake.raw.sh
@@ -3,4 +3,7 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=../install -DBUILD_WITH_WRFIO=ON -DBUILD_WITH_GTG=OFF -DBUILD_WITH_IFI=OFF ../../ 
 make -j 8
+ls -ltr
 make install
+cp ../install/bin/upp.x ../../../upp/exec/upp.x 
+
