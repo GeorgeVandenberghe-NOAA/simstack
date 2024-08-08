51a35ec2f988b8895cd2f9e9e35e2284c2e33c78
george vandenberghe <george.vandenberghe@alogin02.acorn.wcoss2.ncep.noaa.gov>
2024-08-08 16:22:58 +0000

    added support for parallel esmf builds and esmf 8.6.1

--- scr/git.all.sh
+++ scr/git.all.sh
@@ -201,6 +201,7 @@ wget https://github.com/Unidata/netcdf-fortran/archive/v4.5.3.tar.gz ; mv v4.5.3
 wget https://github.com/Unidata/netcdf-fortran/archive/v4.6.0.tar.gz ; mv v4.6.0.tar.gz netcdf-fortran.v4.6.0.tar.gz ; tar -xvf netcdf-fortran.v4.6.0.tar.gz
 wget  https://github.com/esmf-org/esmf/archive/refs/tags/v8.4.1.tar.gz ;  mv  v8.4.1.tar.gz esmf.v8.4.1.tar.gz
 wget  https://github.com/esmf-org/esmf/archive/refs/tags/v8.5.0.tar.gz ;  mv  v8.5.0.tar.gz esmf.v8.5.0.tar.gz
+wget  https://github.com/esmf-org/esmf/archive/refs/tags/v8.6.1.tar.gz ;  mv  v8.6.1.tar.gz esmf.v8.6.1.tar.gz
 
 wget https://github.com/GEOS-ESM/MAPL/archive/refs/tags/v2.40.3.tar.gz  ; mv v2.40.3.tar.gz MAPL.v2.40.3.tar.gz ; tar -xvf MAPL.v2.40.3.tar.gz 
 wget https://github.com/GEOS-ESM/MAPL/archive/refs/tags/v2.35.2.tar.gz  ; mv v2.35.2.tar.gz MAPL.v2.35.2.tar.gz ; tar -xvf MAPL.v2.35.2.tar.gz 
---
+++ scr/make.esmf5.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+export PW=`/bin/pwd`
+rm -rf esmf-8.5.0
+tar -xvf esmf.v8.5.0.tar.gz
+cd esmf-8.5.0
+# ---------------> COMMON PORTION <-------------------
+export NETCDF=$PREFIX
+export HDF5_DIR=$NETCDF
+ if [ $FC == 'ftn' ]
+then
+export ESMF_OS=Unicos
+export ESMF_COMM=mpi
+else
+export ESMF_OS=Linux
+export ESMF_COMM=intelmpi
+fi
+echo $ESMF_OS $ESMF_COMM
+##########3read a
+# ---------------> COMMON PORTION <-------------------
+
+
+export ESMF_NETCDF_LIBS=" -lnetcdff -lnetcdf -L$HDF5_DIR/lib  -lhdf5 -lhdf5_hl -lhdf5 -lz"
+echo $ESMF_NETCDF_LIBS
+sleep 5
+export ESMF_NETCDF_LIBS=" -lnetcdff -lnetcdf -L$HDF5_DIR/lib  -lhdf5 -lhdf5_hl -lhdf5 -lz"
+echo $ESMF_NETCDF_LIBS
+export ename=ESMF_8_5_0
+
+export ESMF_INSTALL_PREFIX=$NETCDF/$ename
+
+
+export ESMF_DIR=`pwd`
+export ESMF_COMPILER=intel
+export ESMF_SITE=default
+export ESMF_INSTALL_MODDIR=mod
+export ESMF_INSTALL_BINDIR=bin
+export ESMF_INSTALL_LIBDIR=lib
+export ESMF_NETCDF=split
+export NETCDF_DIR=$NETCDF
+export ESMF_NETCDF_INCLUDE=$NETCDF_DIR/include
+export ESMF_NETCDF_LIBPATH=$NETCDF_DIR/lib
+export ESMF_F90COMPILEOPTS="-fp-model source"
+export ESMF_CXXCOMPILEOPTS="-fp-model source"
+
+
+gmake clean
+module list
+gmake
+gmake install
+
