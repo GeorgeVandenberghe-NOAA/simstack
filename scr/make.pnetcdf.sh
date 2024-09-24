2fb07c819bb5b0da2515c8526ae707c836404352
George Vandenberghe <gvandenb@hercules-login-2.hpc.msstate.edu>
2024-09-24 10:53:27 -0500

    added pnetcdf library support

---
+++ scr/make.pnetcdf.sh
@@ -0,0 +1,12 @@
+cd pio-2.5.10
+export PW=`/bin/pwd`
+export CMAKE_PREFIX_PATH=$NETP:/tmp/pnetcdf
+cp ./scripts/genf90.pl genf90/./
+rm -rf build
+mkdir build
+cd build
+ cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DNetCDF_PATH=$PREFIX -DPnetCDF_PATH=/tmp/pnetcdf  -DHDF5_PATH=$PREFIX -DCMAKE_VERBOSE_MAKEFILE=1 -DUSER_CMAKE_MODULE_PATH=$PW/pio-2.5.7/CMake_Fortran_utils -DGENF90_PATH=$PW/genf90 -DWITH_PNETCDF=ON -DPIO_ENABLE_TIMING=OFF
+read a
+make -j 1 VERBOSE=1
+make install
+
