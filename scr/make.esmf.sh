e01f8e24e26e1810becf663176956fe009d063ac
george vandenberghe <george.vandenberghe@alogin02.acorn.wcoss2.ncep.noaa.gov>
2024-12-16 20:41:59 +0000

    corrected error in make.esmf.sh

--- scr/make.esmf.sh
+++ scr/make.esmf.sh
@@ -2,7 +2,7 @@ time sh -x make.esmf4.sh  &
 time sh -x make.esmf5.sh  &
 time sh -x make.esmf6.sh  &
 time sh -x make.esmf60.sh  &
-time sh -x make.esmf8..sh  &
+time sh -x make.esmf8.sh  &
 wait
 echo done in $SECONDS seconds
 
