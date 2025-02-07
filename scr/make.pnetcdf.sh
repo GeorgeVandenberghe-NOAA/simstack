3ceb9e50408797d89c76d1fc2c152a9169d9524b
george vandenberghe <george.vandenberghe@alogin02.acorn.wcoss2.ncep.noaa.gov>
2025-02-07 19:47:03 +0000

    modify make.pnetcdf to remove any support for mpi_bcast_c which causes problems on Orion

--- scr/make.pnetcdf.sh
+++ scr/make.pnetcdf.sh
@@ -1,7 +1,16 @@
+export MPIF90=$FC
+export MPIF77=$FC
+export MPICC=$CC
+export MPICXX=$CXX
 rm -rf  pnetcdf-1.13.0
 tar -xvf  pnetcdf-1.13.0.tar.gz
                        cd pnetcdf-1.13.0
+cat configure | sed 's/MPI_Bcast_c/ /g' >configure.new
+mv configure.new configure
+chmod 755 configure
+echo REMOVED MPI_BCAST_C from configure because ufs build cannot find it
                       ./configure --prefix=$PREFIX   --disable-shared --enable-static
                       make -j 1 VERBOSE=1
                       make install
+
 
