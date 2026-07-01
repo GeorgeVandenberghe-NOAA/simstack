f73247b2a32a902dacbf2e43f8a42b47691dd75d
george vandenberghe <george.vandenberghe@alogin02.acorn.wcoss2.ncep.noaa.gov>
2026-07-01 19:33:11 +0000

    added make.pre.sh for convenience and make/.zlib.shared.sh to make a shared zlib elsewhere

---
+++ scr/make.pre.sh
@@ -0,0 +1,6 @@
+sh -x make.zlib.sh
+sh -x make.hdf5.sh
+sh -x make.netcdfcc.sh
+sh -x make.ecbuild.sh
+sh -x make.netcdff*xx.sh
+
---
+++ scr/make.zlib.shared.sh
@@ -0,0 +1,7 @@
+cd zlib-1.2.11
+mkdir build
+ cd build
+ ../configure --prefix=$PREFIX 
+ make -j1
+ make install
+
